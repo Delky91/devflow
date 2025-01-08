b3970f7e6d4a76f564e101536fb16240bde736ba
Luis Miño <l.mino.bustos@gmail.com>
2025-01-08 20:57:21 -0300

    change in eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import _import from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,19 +14,76 @@ const compat = new FlatCompat({
    allConfig: js.configs.all,
 });
 
-const rules = [
-   ...compat.extends(
-      "next/core-web-vitals",
-      "next/typescript",
-      "standard",
-      "plugin:tailwindcss/recommended",
-      "prettier"
+const config = [
+   {
+      ignores: ["components/ui/**/*"],
+   },
+   ...fixupConfigRules(
+      compat.extends(
+         "next/core-web-vitals",
+         "next/typescript",
+         "standard",
+         "plugin:tailwindcss/recommended",
+         "plugin:import/typescript",
+         "prettier"
+      )
    ),
    {
+      plugins: {
+         import: fixupPluginRules(_import),
+      },
+
+      settings: {
+         "import/resolver": {
+            typescript: true,
+            node: true,
+         },
+
+         "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+         },
+      },
+
+      rules: {
+         "import/order": [
+            "error",
+            {
+               groups: [
+                  "builtin",
+                  "external",
+                  "internal",
+                  ["parent", "sibling"],
+                  "index",
+                  "object",
+               ],
+
+               "newlines-between": "always",
+
+               pathGroups: [
+                  {
+                     pattern: "@app/**",
+                     group: "external",
+                     position: "after",
+                  },
+               ],
+
+               pathGroupsExcludedImportTypes: ["builtin"],
+
+               alphabetize: {
+                  order: "asc",
+                  caseInsensitive: true,
+               },
+            },
+         ],
+      },
+   },
+   {
+      files: ["**/*.ts", "**/*.tsx"],
+
       rules: {
          "no-undef": "off",
       },
    },
 ];
 
-export default rules;
+export default config;
