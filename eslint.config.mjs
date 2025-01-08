57307ff44a6a5bb24b7aa8457d6ae2681c30f826
Luis Miño <l.mino.bustos@gmail.com>
2025-01-08 20:33:15 -0300

    vscode setting file and eslint-prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,22 +6,24 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+   baseDirectory: __dirname,
+   recommendedConfig: js.configs.recommended,
+   allConfig: js.configs.all,
 });
 
-export default [
-	...compat.extends(
-		"next/core-web-vitals",
-		"next/typescript",
-		"standard",
-		"plugin:tailwindcss/recommended",
-		"prettier"
-	),
-	{
-		rules: {
-			"no-undef": "off",
-		},
-	},
+const rules = [
+   ...compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+      "standard",
+      "plugin:tailwindcss/recommended",
+      "prettier"
+   ),
+   {
+      rules: {
+         "no-undef": "off",
+      },
+   },
 ];
+
+export default rules;
