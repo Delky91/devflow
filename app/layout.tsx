6346ac0035968af035c083ce91eaec609c06a99e
Luis Miño <l.mino.bustos@gmail.com>
2025-01-13 00:08:08 -0300

    Add: assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,12 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
    title: "DevFlow",
-   description: "My version of stack overflow",
    authors: [{ name: "Luis Miño Bustos" }],
+   description:
+      "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+   icons: {
+      icon: "/images/site-logo.svg",
+   },
 };
 
 export default function RootLayout({
