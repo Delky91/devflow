57c98be22bb3021a5ac8e9e70ffedc3151ac9660
Luis Miño <l.mino.bustos@gmail.com>
2025-01-14 20:29:51 -0300

    Added: autenthication with github and log out, configuration files and forms

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,21 @@
-const Home = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+   const session = await auth();
+
    return (
       <>
          <h1 className=" h1-bold">Welcome to the world of Next.js</h1>
-         <h1 className=" h1-bold font-space-grotesk">
-            Welcome to the world of Next.js
-         </h1>
+         <form
+            action={async () => {
+               "use server";
+               await signOut({ redirectTo: ROUTES.SIGN_IN });
+            }}
+            className="px-10 pt-[100px]">
+            <Button type="submit">Log Out</Button>
+         </form>
       </>
    );
 };
