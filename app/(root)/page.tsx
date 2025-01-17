1b1f44c0b90bb9daf101e5d1fdb390d43cf2c41d
Luis Miño <l.mino.bustos@gmail.com>
2025-01-17 00:26:32 -0300

    Add mobileNavbar, navlinks.ts, new routes with they view, sheet elemt from shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,11 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+// import { auth } from "@/auth";
 
 const Home = async () => {
-   const session = await auth();
+   // const session = await auth();
 
    return (
       <>
          <h1 className=" h1-bold">Welcome to the world of Next.js</h1>
-         <form
-            action={async () => {
-               "use server";
-               await signOut({ redirectTo: ROUTES.SIGN_IN });
-            }}
-            className="px-10 pt-[100px]">
-            <Button type="submit">Log Out</Button>
-         </form>
       </>
    );
 };
---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { JSX } from "react";
+
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants/navLinks";
+import { cn } from "@/lib/utils";
+
+/**
+ * Component that renders navigation links.
+ *
+ * @param {Object} props - The component props.
+ * @param {boolean} [props.isMobileNav=false] - Flag indicating if the navigation is for mobile.
+ *
+ * @returns {JSX.Element} The rendered navigation links.
+ */
+const NavLinks = ({
+   isMobileNav = false,
+}: {
+   isMobileNav?: boolean;
+}): JSX.Element => {
+   const pathname = usePathname();
+   const userId = 1;
+
+   return (
+      <>
+         {sidebarLinks.map((link) => {
+            const isActive =
+               (pathname.includes(link.route) && link.route.length > 1) ||
+               pathname === link.route;
+
+            if (link.route === "/profile") {
+               if (userId) link.route = `/profile/${userId}`;
+               else return null;
+            }
+
+            const LinkComponent = (
+               <Link
+                  href={link.route}
+                  key={link.label}
+                  className={cn(
+                     isActive
+                        ? "primary-gradient rounded-lg text-light-900"
+                        : "text-dark300-light900",
+                     "flex items-center gap-4 bg-transparent p-4"
+                  )}>
+                  <Image
+                     src={link.imgURL}
+                     alt={link.label}
+                     width={20}
+                     height={20}
+                     className={cn({ "invert-colors": !isActive })}
+                  />
+                  <p
+                     className={cn(
+                        isActive ? "base-bold" : "base-medium",
+                        !isMobileNav && "max-lg:hidden"
+                     )}>
+                     {link.label}
+                  </p>
+               </Link>
+            );
+
+            // Wrap the link in a SheetClose component if it's for mobile navigation
+            return isMobileNav ? (
+               <SheetClose
+                  asChild
+                  key={link.route}>
+                  {LinkComponent}
+               </SheetClose>
+            ) : (
+               <React.Fragment key={link.route}>{LinkComponent}</React.Fragment>
+            );
+         })}
+      </>
+   );
+};
+
+export default NavLinks;
