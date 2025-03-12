8062eb82fb9fb9ea587fc274e29dfbd3b89cf610
Luis Miño <l.mino.bustos@gmail.com>
2025-03-12 16:49:49 -0300

    Implemented tag actions

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const TagsPage = () => {
+import { getTags } from "@/lib/actions/tag.actions";
+
+const TagsPage = async () => {
+   const { success, error, data } = await getTags({
+      page: 1,
+      pageSize: 10,
+   });
+
+   const { tags } = data || {};
+   console.log("TAGS", JSON.stringify(tags, null, 2));
+
    return <div>TagsPage</div>;
 };
 
